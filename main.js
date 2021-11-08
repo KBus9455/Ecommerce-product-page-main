0acda69ab12aac5a161e4e95f6108c22717bddb2
kbus9455 <katarzyna.bus9455@gmail.com>
2021-11-08 21:18:37 +0100

    sdd function to show photo and close photo

--- main.js
+++ main.js
@@ -17,10 +17,30 @@ console.log('dziala')
 
 // Display cart 
 
-const cart = document.querySelector('div.cart img');
-const displayCart = document.querySelector('div.displayCart');
+const cartMobile = document.querySelector('div.cart img');
+const cartDesktop = document.querySelector('li.cartTopPanel img');
+const cartContent = document.querySelector('div.displayCart');
 
-cart.addEventListener('click', function(){
-displayCart.classList.toggle('active');
-console.log('dziala')
+const displayCart=function(){
+cartContent.classList.toggle('active');
+}
+cartMobile.addEventListener('click', displayCart);
+cartDesktop.addEventListener('click', displayCart);
+
+// show/hide big foto 
+
+const cross = document.querySelector('div.cross svg');
+const bigPhoto = document.querySelector('.productShow');
+const productPhoto = document.querySelector('img.photoBox');
+
+
+cross.addEventListener('click', function(){
+bigPhoto.classList.remove('active');
 });
+
+productPhoto.addEventListener('click',function(){
+bigPhoto.classList.add('active');
+cartContent.classList.remove('active');
+}); 
+
+
